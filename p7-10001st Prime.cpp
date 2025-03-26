b08a6ae7a14f3b93ecf291194068f07efd2f4385
Siddhartha Bura <136084351+siddhu-1999@users.noreply.github.com>
2025-03-26 17:26:17 +0530

    Update p7-10001st Prime.cpp

--- p7-10001st Prime.cpp
+++ p7-10001st Prime.cpp
@@ -5,9 +5,9 @@ int main() {
     ll n = 10001;
     vector<int> pn(n*n, 1);
     pn[0] = pn[1] = 0;
-    for(ll i=2;i<n;i++)
+    for(ll i=2;i*i<n;i++)
         if(pn[i] == 1)
-        for(ll j=i*i;j<n*n;j+=i)
+        for(ll j=i*i;j<n;j+=i)
             pn[j] = 0;
     ll cnt = 0;
     for(ll i=2;i<pn.size();i++){
